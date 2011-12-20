fd772d96af38308526cedfc01753ad5192ecad20
Dominik Rutkowski <rutdom@iterate.pl>
2011-12-20 16:39:00 +0100

    add components to viev

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -10,7 +10,18 @@ import models.*;
 public class Application extends Controller {
 
     public static void index() {
-        render();
+    	//TODO powitanie jakieś  
+    	
+          List<Notice> freshNotice = Notice.find(
+              "order by postedAt desc"
+          ).fetch(10);
+          render(freshNotice);
     }
 
+    @Before
+    static void addDefaults() {
+        renderArgs.put("blogTitle", Play.configuration.getProperty("blog.title"));
+        renderArgs.put("blogBaseline", Play.configuration.getProperty("blog.baseline"));
+    }
+    
 }
